eb94d403956306452523fbd072649c2b15d8652a
Ari Effendi <ari@arped.id>
2026-03-03 02:18:00 +0700

    Handle getDomain error

--- DNSProviders/Porkbun.php
+++ DNSProviders/Porkbun.php
@@ -109,7 +109,7 @@ class Porkbun extends AbstractDNSProvider
                 'secretapikey' => $this->dnsProvider->credentials['secretapikey'],
             ]);
 
-            if (! $response->successful()) {
+            if (! $response->successful() || $response->json('status') === 'ERROR') {
                 Log::error('Failed to fetch Porkbun domain', ['domainId' => $domainId, 'response' => $response->json()]);
 
                 return [];
