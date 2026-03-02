5e4ab196b9ab244fde4c35726a0f51c4d8118e66
Ari Effendi <ari@arped.id>
2026-03-03 01:15:13 +0700

    Add AbstractDNSProvider

--- DNSProviders/Porkbun.php
+++ DNSProviders/Porkbun.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
+use App\DNSProviders\AbstractDNSProvider;
 use Throwable;
 
 class Porkbun extends AbstractDNSProvider
